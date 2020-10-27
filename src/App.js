4bf7bce7df51e6cc59cf24a0ceaf098a404a5f5a
heidfeld <l.karmanski@gmail.com>
2020-10-27 18:40:46 +0100

    added background

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Navigation from "./components/Navigation/Navigation";
 function App() {
 
   const navigationRows = [
-    {label: 'Test1', url: 'Url1'},
-    {label: 'Test2', url: 'Url2'},
-    {label: 'Test3', url: 'Url3'},
-    {label: 'Test4', url: 'Url4'},
-    {label: 'Test5', url: 'Url5'}
+    {label: 'Coś', url: 'Url1'},
+    {label: 'Albo', url: 'Url2'},
+    {label: 'Ktoś', url: 'Url3'},
+    {label: 'Nie', url: 'Url4'},
+    {label: 'Wiem', url: 'Url5'}
   ];
 
   return (
